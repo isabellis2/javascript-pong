2a2b9b4bf0335dd84ef5a4d289440231e1fdb326
Issas2 <168777599+Issas2@users.noreply.github.com>
2024-06-27 17:42:24 -0300

    cria bolinha 27/06/24

--- pingpong.js
+++ pingpong.js
@@ -1,10 +1,23 @@
+//aqui eu vou criar a bolinha
+let xBolinha = 300;
+let yBolinha = 200;
+let tamBolinha = 25;
 function setup(){
     //aqui vou criar minha "mesa"
     createCanvas(600,400);
+
 }
 
 function draw(){
         //aqui vou por a cor da "mesa"
         //A cor da mesa é em rbg
         background(25,25,112);
+        
+        //chamando a função
+        criaBolinha( xBolinha, yBolinha, tamBolinha);
 }
+
+    //função bolinha
+    function criarBolinha(xBolinha,yBolinha,tamBolinha){
+        circle (xBolinha,yBolinha,tambolinha);
+    }
